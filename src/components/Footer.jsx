7fc798cc90bb5aa729909e0268c9a2fc9c29c7d3
kay <lenguyennhutdinh>
2023-04-14 09:45:01 +0700

    first init

---
+++ src/components/Footer.jsx
@@ -0,0 +1,4 @@
+export default function Footer() {
+    const date = new Date();
+    return <footer><p>Copyright © {date.getFullYear()}</p></footer>
+}
---
+++ src/components/Note.jsx
@@ -0,0 +1,6 @@
+export default function Note() {
+    return <div class="note">
+        <h2>This is title</h2>
+        <p>This is paragraph</p>
+    </div>
+}
